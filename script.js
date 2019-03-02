cc3edec88e7da0609213b8c4d7314388adabcc2e
Ilya Buzunov <46969838+ZTex275@users.noreply.github.com>
2019-03-02 16:10:01 +0300

    Доработана функция окончания игры,
    Добавлено управление с помощью стрелок и мелкие исправления

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', init)
 
 function init(){
-	var input_text, pushb
+	var pushb
 	
-	pushb = document.getElementById('push_btn');
+	pushb = document.getElementById('push_b');
 	pushb.addEventListener('click', pushAdd)
-	
-	input_text = document.getElementById('input_text');
-	input_text.addEventListener('keypress', setInput)
-	
-	document.addEventListener('mousemove', onMouseMove);
 };
 
 function pushAdd() {
@@ -18,17 +13,17 @@ function pushAdd() {
 		var div_el, list
 		list = document.getElementById('containerb')
 		div_el = document.createElement('div');
-		div_el.id ='span';
+		div_el.id = 'span';
 		list.appendChild(div_el)
 		document.addEventListener('mousemove', onMouseMove);
 		for (var i=0 ; i<input_text.value ; i++){
 			var div_sqr = document.createElement('div');
-			div_sqr.style.width ='10px'
-			div_sqr.style.height ='10px'
-			div_sqr.style.backgroundColor ='Red'
+			div_sqr.style.width =  '10px'
+			div_sqr.style.height = '10px'
+			div_sqr.style.backgroundColor = 'Red'
 			div_sqr.style.marginTop = (20 + 'px')
 			div_sqr.style.marginLeft = (30 + 'px')	
-			div_sqr.className ='sqr';
+			div_sqr.className = 'sqr';
 			list.appendChild(div_sqr)}
 	}
 	else{
@@ -40,21 +35,53 @@ list.onmouseover = function(event) {
   var target = event.target;
 
   if (target.className == 'sqr') 		
-			target.style.backgroundColor = "green";
-			if	(target.style.backgroundColor == "green")
-					setTimeout(function(){
+		target.style.backgroundColor = "green";
+
+		if	(div_sqr.style.backgroundColor == "green"){
+				alert("Игра окончена")
 					list.innerHTML='';
-					input_text.value=null;
-					alert("Игра окончена")
-					
-			}, 1000)			
+					input_text.value=null;	
+			}		
 	};				
 }
 
 
 
+//Функция управления стрелками клавиатуры
+
+function moveArrows(e){
+     
+    var s1 = document.getElementById("s1");
+    
+    var cs = window.getComputedStyle(s1);
+     
+    var left = parseInt(cs.marginLeft);
+    var top = parseInt(cs.marginTop);
+     
+    switch(e.keyCode){
+         
+        case 37:  //left
+            if(left>0)
+                s1.style.marginLeft = left - 10 + "px";
+            break;
+        case 38:   //up
+            if(top>0)
+                s1.style.marginTop = top - 10 + "px";
+            break;
+        case 39:   //right
+            if(left < document.documentElement.clientWidth - 100)
+                s1.style.marginLeft = left + 10 + "px";
+            break;
+        case 40:   //down
+            if(top < document.documentElement.clientHeight - 100)
+                s1.style.marginTop = top + 10 + "px";
+            break;
+    }
+}
+addEventListener("keydown", moveArrows);
 
 
+//Функция управления мышью
 
 var timeout = null;
 	
